e7c27c4facf4b3fae414e2ea4143f220d059b4b1
kylian moreira <kylianmoreira@gmail.com>
2022-10-24 10:32:05 +0200

    add route post

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 
 const app = express();
-
+app.use(express.json());
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -17,6 +17,7 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovies);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
